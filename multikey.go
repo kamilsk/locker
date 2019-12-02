b87c8cb0c1c6351df6892c9008821673c6c88524
Kamil Samigullin <kamil@samigullin.info>
2019-12-02 14:56:54 +0300

    support rw mutex

--- multikey.go
+++ multikey.go
@@ -1,13 +1,14 @@
 package locker
 
 import (
-	"crypto/md5"
+	"encoding/hex"
 	"hash"
+	"math/big"
 	"sync"
 )
 
-func MultiMutex(capacity int) multimx {
-	return multimx{hash: md5.New(), set: make([]sync.Mutex, capacity)}
+func MultiMutex(capacity int, hash hash.Hash) multimx {
+	return multimx{hash: hash, set: make([]sync.Mutex, capacity)}
 }
 
 type multimx struct {
@@ -15,11 +16,19 @@ type multimx struct {
 	set  []sync.Mutex
 }
 
-func (mx multimx) Mutex(key string) *sync.Mutex {
-	var spot int
-	for _, point := range mx.hash.Sum([]byte(key)) {
-		spot += int(point)
-	}
-	spot %= len(mx.set)
-	return &mx.set[spot]
+func (mx multimx) ByFingerprint(fingerprint []byte) *sync.Mutex {
+	_, _ = mx.hash.Write(fingerprint)
+	base := big.NewInt(0)
+	_, _ = base.SetString(hex.EncodeToString(mx.hash.Sum(nil)), 16)
+	mx.hash.Reset()
+	shard := big.NewInt(0).Mod(base, big.NewInt(int64(len(mx.set))))
+	return mx.ByVirtualShard(shard.Uint64())
+}
+
+func (mx multimx) ByKey(key string) *sync.Mutex {
+	return mx.ByFingerprint([]byte(key))
+}
+
+func (mx multimx) ByVirtualShard(shard uint64) *sync.Mutex {
+	return &mx.set[shard%uint64(len(mx.set))]
 }
--- multikey_test.go
+++ multikey_test.go
@@ -1,19 +1,57 @@
 package locker_test
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
+	"hash/fnv"
+	"runtime"
 	"testing"
 
-	"github.com/kamilsk/locker"
+	. "github.com/kamilsk/locker"
 )
 
 func TestMultiMutex(t *testing.T) {
-	set := locker.MultiMutex(3)
-	set.Mutex("test").Lock()
-	set.Mutex("another").Lock()
-	defer set.Mutex("test").Unlock()
-	defer set.Mutex("another").Unlock()
-	go func() { set.Mutex("test").Unlock() }()
-	go func() { set.Mutex("another").Unlock() }()
-	set.Mutex("test").Lock()
-	set.Mutex("another").Lock()
+	set := MultiMutex(3, md5.New())
+	set.ByKey("test").Lock()
+	set.ByKey("another").Lock()
+	defer set.ByKey("test").Unlock()
+	defer set.ByKey("another").Unlock()
+	go func() { set.ByKey("test").Unlock() }()
+	go func() { set.ByKey("another").Unlock() }()
+	set.ByKey("test").Lock()
+	set.ByKey("another").Lock()
+}
+
+// BenchmarkMultiMutex/md5-4         	  340592	      3822 ns/op	     712 B/op	      29 allocs/op
+// BenchmarkMultiMutex/sha1-4        	  296442	      4391 ns/op	     912 B/op	      30 allocs/op
+// BenchmarkMultiMutex/sha256-4      	  205178	      5874 ns/op	    1000 B/op	      29 allocs/op
+// BenchmarkMultiMutex/sha512-4      	  131266	     10442 ns/op	    1864 B/op	      32 allocs/op
+// BenchmarkMultiMutex/sum32-4       	  626733	      1696 ns/op	     288 B/op	      27 allocs/op
+func BenchmarkMultiMutex(b *testing.B) {
+	benchmarks := []struct {
+		name string
+		hash hash.Hash
+	}{
+		{name: "md5", hash: md5.New()},
+		{name: "sha1", hash: sha1.New()},
+		{name: "sha256", hash: sha256.New()},
+		{name: "sha512", hash: sha512.New()},
+		{name: "sum32", hash: fnv.New32()},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			b.ReportAllocs()
+
+			keys := []string{runtime.GOOS, runtime.GOARCH, runtime.Version()}
+			set := MultiMutex(10, bm.hash)
+			for i := 0; i < b.N; i++ {
+				for _, key := range keys {
+					_ = set.ByKey(key)
+				}
+			}
+		})
+	}
 }
