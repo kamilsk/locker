155b961e2a46e782470ff0a50459cb5d8399fcd6
Kamil Samigullin <kamil@samigullin.info>
2019-12-06 22:29:33 +0300

    complete ShardNumber calculation

--- internal/shard.go
+++ internal/shard.go
@@ -5,34 +5,42 @@ import (
 	"math/big"
 )
 
-func ShardNumberNaive(in []byte, size uint64) uint64 {
-	base, success := big.NewInt(0).SetString(hex.EncodeToString(in), 16)
-	if !success {
-		panic("invalid input")
-	}
+// ShardNumberNaive calculates a shard number by a hash/fingerprint of a custom payload
+// and a size of an available mapping set.
+//
+// This is a naive implementation to control the correctness of the simple implementation.
+func ShardNumberNaive(checksum []byte, size uint64) uint64 {
+	base, _ := big.NewInt(0).SetString(hex.EncodeToString(checksum), 16)
 	return big.NewInt(0).Mod(base, big.NewInt(0).SetUint64(size)).Uint64()
 }
 
-func ShardNumberSimple(in []byte, size uint64) uint64 {
-	return big.NewInt(0).Mod(big.NewInt(0).SetBytes(in), big.NewInt(0).SetUint64(size)).Uint64()
+// ShardNumberNaive calculates a shard number by a hash/fingerprint of a custom payload
+// and a size of an available mapping set.
+//
+// This is a simple implementation to control the correctness of the fast implementation.
+func ShardNumberSimple(checksum []byte, size uint64) uint64 {
+	return big.NewInt(0).Mod(big.NewInt(0).SetBytes(checksum), big.NewInt(0).SetUint64(size)).Uint64()
 }
 
-func ShardNumberFast(in []byte, size uint64) uint64 {
-	shard := uint64(0)
-	for f, i := uint64(1), len(in)-1; i >= 0; i-- {
-		curByte := in[i]
+// ShardNumberNaive calculates a shard number by a hash/fingerprint of a custom payload
+// and a size of an available mapping set.
+//
+// This is a fast implementation without disadvantages of the simple implementation.
+func ShardNumberFast(checksum []byte, size uint64) uint64 {
+	var shard, factor uint64 = 0, 1
+	for i := len(checksum) - 1; i >= 0; i-- {
+		curByte := checksum[i]
 		for j := 0; j < 8; j++ {
 			// we iterate over bits to use sum instead of multiplication for the factor:
 			// on each iteration: factor = factor * 2 % size <=> (factor + factor) % size
 			bit := curByte % 2
 			curByte /= 2
 			if bit == 1 {
-				shard = sumModWithoutOverflow(shard, f, size)
+				shard = sumModWithoutOverflow(shard, factor, size)
 			}
-			f = sumModWithoutOverflow(f, f, size)
+			factor = sumModWithoutOverflow(factor, factor, size)
 		}
 	}
-
 	return shard
 }
 
--- internal/shard_test.go
+++ internal/shard_test.go
@@ -8,16 +8,16 @@ import (
 	"hash"
 	"hash/fnv"
 	"math"
+	"math/rand"
 	"runtime"
 	"testing"
+	"time"
 
 	. "github.com/kamilsk/locker/internal"
 )
 
-var keys = [...]string{runtime.GOOS, runtime.GOARCH}
-
 func TestShardNumberCalculation(t *testing.T) {
-	sources := map[string]hash.Hash{
+	hashes := map[string]hash.Hash{
 		"md5":     md5.New(),
 		"sha1":    sha1.New(),
 		"sha256":  sha256.New(),
@@ -29,94 +29,78 @@ func TestShardNumberCalculation(t *testing.T) {
 		"sum128":  fnv.New128(),
 		"sum128a": fnv.New128a(),
 	}
-	size := uint64(math.MaxUint64)
-	for name, checksum := range sources {
+	keys := [...][]byte{[]byte(runtime.GOOS), []byte(runtime.GOARCH)}
+	sizes := []uint64{
+		math.MaxUint8, math.MaxUint16, math.MaxUint32, math.MaxUint64,
+		math.MaxUint8 * (1 + math.MaxUint8), rand.New(rand.NewSource(time.Now().UnixNano())).Uint64(),
+	}
+	for name, algorithm := range hashes {
 		t.Run(name, func(t *testing.T) {
 			for _, key := range keys {
-				checksum.Write([]byte(key))
-				in := checksum.Sum(nil)
-				checksum.Reset()
+				algorithm.Write(key)
+				checksum := algorithm.Sum(nil)
+				algorithm.Reset()
 
-				x, y := ShardNumberNaive(in, size), ShardNumberSimple(in, size)
-				if x != y {
-					t.Errorf("%d != %d", x, y)
-					t.FailNow()
-				}
+				for _, size := range sizes {
+					x, y := ShardNumberNaive(checksum, size), ShardNumberSimple(checksum, size)
+					if x != y {
+						t.Errorf("%d != %d", x, y)
+						t.FailNow()
+					}
 
-				z := ShardNumberFast(in, size)
-				if x != z {
-					t.Errorf("%d != %d", x, z)
-					t.FailNow()
+					z := ShardNumberFast(checksum, size)
+					if x != z {
+						t.Errorf("%d != %d", x, z)
+						t.FailNow()
+					}
 				}
 			}
 		})
 	}
 }
 
-// BenchmarkShardNumberCalculation/naive,md5:darwin-12         	 3000000	       575 ns/op	     216 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,md5:amd64-12          	 3000000	       560 ns/op	     216 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sha1:darwin-12        	 2000000	       644 ns/op	     264 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sha1:amd64-12         	 2000000	       647 ns/op	     264 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sha256:darwin-12      	 2000000	       868 ns/op	     296 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sha256:amd64-12       	 2000000	       849 ns/op	     296 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sha512:darwin-12      	 1000000	      1512 ns/op	     552 B/op	       9 allocs/op
-// BenchmarkShardNumberCalculation/naive,sha512:amd64-12       	 1000000	      1473 ns/op	     552 B/op	       9 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum32:darwin-12       	 5000000	       264 ns/op	      72 B/op	       6 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum32:amd64-12        	 5000000	       265 ns/op	      72 B/op	       6 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum32a:darwin-12      	 5000000	       263 ns/op	      72 B/op	       6 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum32a:amd64-12       	 5000000	       269 ns/op	      72 B/op	       6 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum64:darwin-12       	 5000000	       369 ns/op	     136 B/op	       7 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum64:amd64-12        	 5000000	       370 ns/op	     136 B/op	       7 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum64a:darwin-12      	 3000000	       368 ns/op	     136 B/op	       7 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum64a:amd64-12       	 5000000	       373 ns/op	     136 B/op	       7 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum128:darwin-12      	 3000000	       560 ns/op	     216 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum128:amd64-12       	 3000000	       564 ns/op	     216 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum128a:darwin-12     	 3000000	       557 ns/op	     216 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/naive,sum128a:amd64-12      	 3000000	       586 ns/op	     216 B/op	       8 allocs/op
-// BenchmarkShardNumberCalculation/simple,md5:darwin-12        	10000000	       174 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,md5:amd64-12         	10000000	       166 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sha1:darwin-12       	10000000	       196 ns/op	     144 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sha1:amd64-12        	10000000	       203 ns/op	     144 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sha256:darwin-12     	10000000	       222 ns/op	     144 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sha256:amd64-12      	10000000	       233 ns/op	     144 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sha512:darwin-12     	 5000000	       346 ns/op	     208 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sha512:amd64-12      	 5000000	       354 ns/op	     208 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum32:darwin-12      	20000000	        97.8 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum32:amd64-12       	20000000	        95.9 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum32a:darwin-12     	20000000	        93.0 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum32a:amd64-12      	20000000	        95.1 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum64:darwin-12      	20000000	        87.2 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum64:amd64-12       	20000000	        86.5 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum64a:darwin-12     	20000000	        86.3 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum64a:amd64-12      	20000000	        86.2 ns/op	      24 B/op	       3 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum128:darwin-12     	10000000	       157 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum128:amd64-12      	10000000	       158 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum128a:darwin-12    	10000000	       157 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/simple,sum128a:amd64-12     	10000000	       157 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkShardNumberCalculation/fast,md5:darwin-12          	10000000	       177 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,md5:amd64-12           	10000000	       175 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sha1:darwin-12         	10000000	       217 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sha1:amd64-12          	10000000	       221 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sha256:darwin-12       	 5000000	       348 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sha256:amd64-12        	 5000000	       346 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sha512:darwin-12       	 2000000	       701 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sha512:amd64-12        	 2000000	       701 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum32:darwin-12        	30000000	        45.4 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum32:amd64-12         	30000000	        45.7 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum32a:darwin-12       	30000000	        44.8 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum32a:amd64-12        	30000000	        45.1 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum64:darwin-12        	20000000	        89.4 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum64:amd64-12         	20000000	        91.2 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum64a:darwin-12       	20000000	        88.6 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum64a:amd64-12        	20000000	        88.8 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum128:darwin-12       	10000000	       174 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum128:amd64-12        	10000000	       174 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum128a:darwin-12      	10000000	       175 ns/op	       0 B/op	       0 allocs/op
-// BenchmarkShardNumberCalculation/fast,sum128a:amd64-12       	10000000	       174 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/naive:md5-4         	 1000000	      1011 ns/op	     216 B/op	       8 allocs/op
+// BenchmarkShardNumberCalculation/naive:sha1-4        	 1000000	      1166 ns/op	     264 B/op	       8 allocs/op
+// BenchmarkShardNumberCalculation/naive:sha256-4      	  583350	      1770 ns/op	     296 B/op	       8 allocs/op
+// BenchmarkShardNumberCalculation/naive:sha512-4      	  382222	      2687 ns/op	     552 B/op	       9 allocs/op
+// BenchmarkShardNumberCalculation/naive:sum32-4       	 2314318	       463 ns/op	      72 B/op	       6 allocs/op
+// BenchmarkShardNumberCalculation/naive:sum32a-4      	 2239576	       466 ns/op	      72 B/op	       6 allocs/op
+// BenchmarkShardNumberCalculation/naive:sum64-4       	 1904371	       616 ns/op	     136 B/op	       7 allocs/op
+// BenchmarkShardNumberCalculation/naive:sum64a-4      	 1798017	       662 ns/op	     136 B/op	       7 allocs/op
+// BenchmarkShardNumberCalculation/naive:sum128-4      	 1000000	      1076 ns/op	     216 B/op	       8 allocs/op
+// BenchmarkShardNumberCalculation/naive:sum128a-4     	 1219659	       911 ns/op	     216 B/op	       8 allocs/op
+// BenchmarkShardNumberCalculation/simple:md5-4        	 5383893	       223 ns/op	     112 B/op	       4 allocs/op
+// BenchmarkShardNumberCalculation/simple:sha1-4       	 4214607	       283 ns/op	     144 B/op	       4 allocs/op
+// BenchmarkShardNumberCalculation/simple:sha256-4     	 4009988	       287 ns/op	     144 B/op	       4 allocs/op
+// BenchmarkShardNumberCalculation/simple:sha512-4     	 2707340	       417 ns/op	     208 B/op	       4 allocs/op
+// BenchmarkShardNumberCalculation/simple:sum32-4      	 8844078	       129 ns/op	      24 B/op	       3 allocs/op
+// BenchmarkShardNumberCalculation/simple:sum32a-4     	 8814894	       132 ns/op	      24 B/op	       3 allocs/op
+// BenchmarkShardNumberCalculation/simple:sum64-4      	 9100696	       128 ns/op	      24 B/op	       3 allocs/op
+// BenchmarkShardNumberCalculation/simple:sum64a-4     	 8227677	       130 ns/op	      24 B/op	       3 allocs/op
+// BenchmarkShardNumberCalculation/simple:sum128-4     	 4863288	       227 ns/op	     112 B/op	       4 allocs/op
+// BenchmarkShardNumberCalculation/simple:sum128a-4    	 5061507	       225 ns/op	     112 B/op	       4 allocs/op
+// BenchmarkShardNumberCalculation/fast:md5-4          	 3878890	       304 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sha1-4         	 3090046	       381 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sha256-4       	 1827475	       651 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sha512-4       	  839475	      1368 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sum32-4        	18319796	        63.1 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sum32a-4       	19422997	        60.8 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sum64-4        	 9456823	       125 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sum64a-4       	 9441171	       128 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sum128-4       	 3846472	       310 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkShardNumberCalculation/fast:sum128a-4      	 3861493	       308 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkShardNumberCalculation(b *testing.B) {
+	benchmarks := []struct {
+		name           string
+		implementation func([]byte, uint64) uint64
+	}{
+		{"naive", ShardNumberNaive},
+		{"simple", ShardNumberSimple},
+		{"fast", ShardNumberFast},
+	}
 	hashes := []struct {
-		name     string
-		checksum hash.Hash
+		name string
+		hash hash.Hash
 	}{
 		{"md5", md5.New()},
 		{"sha1", sha1.New()},
@@ -129,30 +113,20 @@ func BenchmarkShardNumberCalculation(b *testing.B) {
 		{"sum128", fnv.New128()},
 		{"sum128a", fnv.New128a()},
 	}
-	benchmarks := []struct {
-		name           string
-		implementation func([]byte, uint64) uint64
-	}{
-		{"naive", ShardNumberNaive},
-		{"simple", ShardNumberSimple},
-		{"fast", ShardNumberFast},
-	}
-	size := uint64(math.MaxUint64)
+	key, size := []byte(runtime.Version()), rand.New(rand.NewSource(time.Now().UnixNano())).Uint64()
 	for _, benchmark := range benchmarks {
 		for _, algorithm := range hashes {
-			for _, key := range keys {
-				b.Run(benchmark.name+","+algorithm.name+":"+key, func(b *testing.B) {
-					algorithm.checksum.Write([]byte(key))
-					in := algorithm.checksum.Sum(nil)
-					algorithm.checksum.Reset()
+			b.Run(benchmark.name+":"+algorithm.name, func(b *testing.B) {
+				algorithm.hash.Write(key)
+				checksum := algorithm.hash.Sum(nil)
+				algorithm.hash.Reset()
 
-					b.ResetTimer()
-					b.ReportAllocs()
-					for i := 0; i < b.N; i++ {
-						_ = benchmark.implementation(in, size)
-					}
-				})
-			}
+				b.ResetTimer()
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					_ = benchmark.implementation(checksum, size)
+				}
+			})
 		}
 	}
 }
